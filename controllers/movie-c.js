f16f7f5101b67266976759046c5142320a96621e
MD. Shakib Khan <148666420+Projects-Materials@users.noreply.github.com>
2024-03-19 12:30:23 +0600

    a

--- controllers/movie-c.js
+++ controllers/movie-c.js
@@ -3,12 +3,12 @@ const client = require("../client.js");
 module.exports =  conc = async(req,res)=>{
     try{
         const cached_value = await client.get('cached_value');
-        if(cached_value) return res.status(200).json(movies);
+        if(cached_value) return res.status(200).json(JSON.parse(cached_value));
         const movies = await collection.trendingMovies(1);
-        await client.set('cached_value', movies);
+        await client.set('cached_value', JSON.stringify(movies));
         await client.expire('cached_value', 20);
         res.status(200).json(movies)
     }catch(error){
-        res.status(500).json({message: error.message});
+        res.status(500).json({error: error.message});
     }
 }
