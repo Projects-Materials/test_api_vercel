0f9ebb119605a150fbcb8b973cdbea26b724b63b
MD. Shakib Khan <148666420+Projects-Materials@users.noreply.github.com>
2024-03-19 12:54:14 +0600

    1

--- controllers/movie-c.js
+++ controllers/movie-c.js
@@ -2,12 +2,11 @@ const { collection } = require("sigma-error");
 const client = require("../client.js");
 module.exports =  conc = async(req,res)=>{
     try{
-        const cached_value = await client.get('cached_value');
-        if(cached_value) return res.status(200).json(JSON.parse(cached_value));
-        console.log("Hello world");
+        // const cached_value = await client.get('cached_value');
+        // if(cached_value) return res.status(200).json(JSON.parse(cached_value));
         const movies = await collection.trendingMovies(1);
-        await client.set('cached_value', JSON.stringify(movies));
-        await client.expire('cached_value', 20);
+        // await client.set('cached_value', JSON.stringify(movies));
+        // await client.expire('cached_value', 20);
         res.status(200).json(movies)
     }catch(error){
         res.status(500).json({error: error.message});
